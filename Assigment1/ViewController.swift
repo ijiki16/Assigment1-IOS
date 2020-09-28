47053f884476558ddf525b073f10dfa9ac585e56
iuri <ijiki16@freeuni.edu.ge>
2020-09-28 17:29:17 +0400

    Initial Commit

---
+++ Assigment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assigment1
+//
+//  Created by iuri jikidze on 9/28/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
