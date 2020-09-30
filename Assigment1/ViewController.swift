9f5a22fa0ce850ca7d4b67ab2ba5c30e1aa6a159
iuri <ijiki16@freeuni.edu.ge>
2020-09-30 16:26:35 +0400

    view random color

--- Assigment1/ViewController.swift
+++ Assigment1/ViewController.swift
@@ -9,9 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet var arrayOfSevenSegments: [UIView]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        DispatchQueue.main.asyncAfter (deadline: .now() + .seconds(5)){
+            for sevenSegment in self.arrayOfSevenSegments{
+//                sevenSegment.backgroundColor = .blue
+                for segment in sevenSegment.subviews{
+                    segment.backgroundColor = self.randomColor()
+                }
+            }
+        }
+        
         // Do any additional setup after loading the view.
+    }
+    
+    
+    func randomColor() -> UIColor{
+        UIColor(red: CGFloat(arc4random_uniform(256))/255,
+                green: CGFloat(arc4random_uniform(256))/255,
+                blue: CGFloat(arc4random_uniform(256))/255,
+                alpha: 1.0)
     }
 
 
